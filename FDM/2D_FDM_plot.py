d0009a348f9eeeef919de2644a44ff2b2e9e128b
Julius Weidinger <julius.weidinger@tum.de>
2024-12-11 10:53:01 +0100

    Replace 2D_FDM_plot.py

--- FDM/2D_FDM_plot.py
+++ FDM/2D_FDM_plot.py
@@ -10,7 +10,6 @@ yMin = 0
 yMax = 10
 yPoints = 200
 
-
 xLength = xMax - xMin
 yLength = yMax - yMin
 x = np.linspace(xMin, xMax, xPoints)
@@ -20,27 +19,34 @@ dy = yLength / (yPoints -1)
 
 # time
 time = 2.0
-timeSteps = 2500
-dt = time / (timeSteps - 1)
+dt = 1e-2
+timeSteps = int(time / dt) + 1
 
-# wave speed
-c = 25
+# material parameters
+density = 1.0
+wavespeed = 1.0
 
-# spatially varying density
+# initial displacement
+frequency = 1.0
+lamda = wavespeed / frequency
+sigma = lamda / 2 / np.pi
+
+# density
 rho = np.zeros((xPoints, yPoints))
 for i in range(xPoints):
     for j in range(yPoints):
-        rho[i, j] = 1.0 # - 0.005 * abs(x[i] - 0.5 * xLength) - 0.005 * abs(y[j] - 0.5 * yLength)
+        rho[i, j] = density
 
-for i in range(20,80):
-    for j in range(20,80):
-        rho[i,j] = 1e-5
+# create damaged area
+#for i in range(20,80):
+#    for j in range(20,80):
+#        rho[i,j] = 1e-5
 
-# stability (CFL condition)
-if (c * dt) / dx > 1.0 or (c * dt) / dy > 1.0:
+# stability - CFL condition
+if (wavespeed * dt) / dx > 1.0 or (wavespeed * dt) / dy > 1.0:
     raise ValueError("CLF condition is not satisfied!")
+print("CFL value: ", (wavespeed * dt) / dx)
 
-print((c*dt)/dx)
 # initialize arrays
 u = np.zeros((xPoints, yPoints))
 u_old = np.zeros((xPoints, yPoints))
@@ -49,7 +55,6 @@ u_new = np.zeros((xPoints, yPoints))
 # initial condition
 x0 = xLength / 2
 y0 = yLength / 2
-sigma = 0.1
 for i in range(xPoints):
     for j in range(yPoints):
         u[i, j] = 2*np.exp(-((x[i] - x0) ** 2 + (y[j] - y0) ** 2) / (2 * sigma ** 2))
@@ -63,7 +68,6 @@ X, Y = np.meshgrid(x, y)
 color_mesh = ax.pcolormesh(X, Y, u.T, shading='auto', cmap='seismic', vmin=-1, vmax=1)
 ax.set_xlim(xMin, xMax)
 ax.set_ylim(yMin, yMax)
-ax.set_xlabel('x')
 fig.colorbar(color_mesh, ax=ax)
 
 # update function
@@ -84,28 +88,28 @@ def animate(frame):
             y_term1 = y_rho_half_plus * (u[i, j + 1] - u[i, j])
             y_term2 = y_rho_half_minus * (u[i, j] - u[i, j - 1])
 
-            u_new[i, j] = 2 * u[i, j] - u_old[i, j] + ((dt ** 2 * c**2) / (rho[i,j] * dx ** 2)) * (x_term1 - x_term2) + ((dt ** 2 * c**2) / (rho[i,j] * dy ** 2)) * (y_term1 - y_term2)
+            u_new[i, j] = 2 * u[i, j] - u_old[i, j] + ((dt ** 2 * wavespeed**2) / (rho[i,j] * dx ** 2)) * (x_term1 - x_term2) + ((dt ** 2 * wavespeed**2) / (rho[i,j] * dy ** 2)) * (y_term1 - y_term2)
 
 
-    # boundary condition (Dirichlet)
+    # Dirichlet boundary condition
     u_new[0, :] = 0
     u_new[:, 0] = 0
     u_new[-1, :] = 0
     u_new[:, -1] = 0
 
-    '''
-    # boundary condition (Neumann)
+
+    # Neumann boundary condition
     # corners
-    u_new[0, 0] = u_new[1, 1]
-    u_new[0, -1] = u_new[1, -2]
-    u_new[-1, 0] = u_new[-2, 1]
-    u_new[-1, -1] = u_new[-2, -2]
+    #u_new[0, 0] = u_new[1, 1]
+    #u_new[0, -1] = u_new[1, -2]
+    #u_new[-1, 0] = u_new[-2, 1]
+    #u_new[-1, -1] = u_new[-2, -2]
     # edges
-    u_new[0, 1:-1] = u_new[1, 1:-1]
-    u_new[-1, 1:-1] = u_new[-2, 1:-1]
-    u_new[1:-1, 0] = u_new[1:-1, 1]
-    u_new[1:-1, -1] = u_new[1:-1, -2]
-    '''
+    #u_new[0, 1:-1] = u_new[1, 1:-1]
+    #u_new[-1, 1:-1] = u_new[-2, 1:-1]
+    #u_new[1:-1, 0] = u_new[1:-1, 1]
+    #u_new[1:-1, -1] = u_new[1:-1, -2]
+
 
     # update time step
     u_old[:, :] = u[:, :]
@@ -116,6 +120,7 @@ def animate(frame):
 
 
 # animation
-ani = animation.FuncAnimation(fig, animate, frames=timeSteps, blit=True, interval=0.0)
+ani = animation.FuncAnimation(fig, animate, frames=range(0, timeSteps, 100), blit=True, interval=100.0)
 plt.xlabel('x')
+plt.ylabel('y')
 plt.show()
